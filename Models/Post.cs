81b199add321d068df0a9825f4a2c5cf370a38e3
Yang0Jin0Woo <juyang00@gmail.com>
2026-02-09 13:38:13 +0900

    Revert "feat: UTC<=>KST + DI, 컨텍스트 관리, 타임스탬프, 조회 전용 및 수정 전용 분리 설명 추가"
    This reverts commit 37143f0996c41848286196f3c356b1bad2644382.

--- Models/Post.cs
+++ Models/Post.cs
@@ -4,9 +4,6 @@ namespace BoardApp.Models
 {
     public class Post
     {
-        private static readonly TimeZoneInfo KstTimeZone =
-            TimeZoneInfo.FindSystemTimeZoneById("Korea Standard Time");
-
         public int Id { get; set; }
 
         public string Title { get; set; } = "";
@@ -15,11 +12,5 @@ namespace BoardApp.Models
 
         public DateTime CreatedAtUtc { get; set; }
         public DateTime UpdatedAtUtc { get; set; }
-
-        public DateTime CreatedAtKst =>
-            TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(CreatedAtUtc, DateTimeKind.Utc), KstTimeZone);
-
-        public DateTime UpdatedAtKst =>
-            TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(UpdatedAtUtc, DateTimeKind.Utc), KstTimeZone);
     }
 }
